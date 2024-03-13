a46fd0db286de67c4dc8e89638c8e204dde17a54
maddrills <mathewfvv@gmail.com>
2024-03-13 12:53:31 +0530

    Day 2

---
+++ day2/dataTypes/main.py
@@ -0,0 +1,29 @@
+# H
+print("Hello"[0])
+
+# redability you can add _ op will be 554545222
+
+print(554_545_222);
+
+
+# find the data type of something
+# <class 'str'>
+# <class 'int'>
+print(type("Hello"))
+print(type(543234))
+
+# print power 2 ^ 5
+print(2 ** 5)
+power = 2 ** 5
+
+fancyDigit = 554_545_222;
+
+# format a print f before quotes
+print(f"power {power} and fancy digits are {fancyDigit}")
+
+#NOTE in python +"" doesnt covert a datatype to string like in java
+# print(30+"") -> error
+
+# round
+x = round(5.76543, 2)
+print(x)
