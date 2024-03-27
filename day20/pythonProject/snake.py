af51f0a4ee68ee3c3e391071a8f0bc2707a9a3f9
maddrills <mathewfvv@gmail.com>
2024-03-27 09:31:28 +0530

    D20 snake game start classes

---
+++ day20/pythonProject/snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        game_id_on = True
+
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # new_x = segments[seg_num - 1].xcor()
+            # new_y = segments[seg_num - 1].xcor()
+
+            snake_pos = self.segments[seg_num - 1].position()
+            self.segments[seg_num].goto(snake_pos)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
