eb0c304c14ea1c056eba9b461b06b6bd7c09ebe7
maddrills <mathewfvv@gmail.com>
2024-04-05 09:25:22 +0530

    Day 30 exception

---
+++ day27 try catch finally/main.py
@@ -0,0 +1,24 @@
+def tryCatchFunction():
+    try:
+        file = open("a_file.txt")
+        a_dictionary = {"key": "value"}
+        print(a_dictionary["randomKey"])
+    except FileNotFoundError:
+        file = open("a_file.txt","w")
+        file.write("Something")
+    except KeyError as error_message:
+        print(f"Key Error {error_message}")
+    else:
+        content = file.read()
+        print(content)
+    finally:
+        file.close()
+        print("file was close.")
+
+
+
+
+
+if __name__ == "__main__":
+    tryCatchFunction()
+    print("main call")
